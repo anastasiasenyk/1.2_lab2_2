819b5a786bc74c59e2eb551a4ec0f49244ad9e4f
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-02-20 01:47:27 +0200

    new func

---
+++ twitter_api_2.py
@@ -0,0 +1,84 @@
+"""json navigation"""
+import json
+import os.path
+import time
+
+
+def information(data, name):
+    """
+    find all dictionary keys and print them
+    :param data: list/dict
+    :param name: str
+    :return: None
+    """
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                information(value, name + '/' + str(key))
+            elif value != [] and isinstance(value, list) and isinstance(value[0], dict):
+                for item in value:
+                    information(item, name + '/' + str(key))
+            else:
+                print((name + '/' + str(key))[1:])
+    elif data != [] and isinstance(data, list) and isinstance(data[0], dict):
+        for item in data:
+            information(item, name)
+
+
+def unpacking(data, path: list):
+    """
+    find value by the key in dict
+    :param data: list/dict
+    :param path: str
+    :return: str
+    """
+    if path:
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if path and str(key) == path[0]:
+                    path2 = path[1:]
+                    if path2:
+                        new_value = unpacking(value, path2)
+                        if new_value:
+                            return str(new_value)
+                    else:
+                        return str(value)
+        elif isinstance(data, list):
+            for item in data:
+                new_value = unpacking(item, path)
+                if new_value:
+                    return new_value
+    return
+
+
+if __name__ == '__main__':
+    while True:
+        print('Введіть назву файлу у вигляді /path_to_file/name_file.json')
+        user = input('>>> ')
+        if user == 'quit' or os.path.exists(user) and user.split('.')[-1] == 'json':
+            break
+        else:
+            print('Ваш файл не існує або не являється файлом json.'
+                  ' Cпробуйте ще раз, або введіть "quit", якщо бажаєте вийти.')
+
+    if user != 'quit':
+        try:
+            with open(user, 'r') as file:
+                file1 = json.load(file)
+            print('Набір можливих шляхів до ключів словника')
+            time.sleep(2)
+        except:
+            user = 'quit'
+            print('Ваш файл не вдається відкрити.')
+
+    if user != 'quit':
+        information(file1, '')
+        print('Введіть один із поданих ключів')
+        while True:
+            user = input('>>> ')
+            if user != 'quit':
+                if user[0] == '':
+                    user = user[1:]
+                value = str(unpacking(file1, user.split('/')))
+            else:
+                break
